216bacdf4cf02657bd1a9a17cd63bb184debc7d5
Gilberto <gilberto.braga0021@gmail.com>
2023-10-14 21:47:56 -0300

    Commite inicial

---
+++ conversor.py
@@ -0,0 +1,4 @@
+# Lógica do conversor de temperatura
+
+def cel_fah(temp):
+    return 9 * temp / 5 + 32
---
+++ main.py
@@ -0,0 +1,10 @@
+from conversor import cel_fah
+
+# Conversor de temperatura
+print("\n\t\t\t -- Conversor de Temperatura -- \n\n")
+
+temp = int(input("Informe a temperatura: "))
+
+fah = cel_fah(temp)
+
+print(f'\n\n\t{temp}ºC = {fah}ºF\n\n')
